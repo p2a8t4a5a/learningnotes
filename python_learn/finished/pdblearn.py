da684dfae53e89be382d9bfc84a24102529c7069
dongen.yao <dongen.yao@cootek.cn>
2017-02-16 16:27:25 +0800

    update

---
+++ python_learn/finished/pdblearn.py
@@ -0,0 +1,28 @@
+# -*- coding:utf8 -*-
+
+
+"""
+n 下一条语句
+c 程序继续执行
+p value 输出变量value
+q 退出
+list 显示代码
+s 进入函数
+r 退出函数
+value = 123 修改变量 
+"""
+
+
+
+
+import pdb
+
+a= "aaa"
+pdb.set_trace()
+
+b="bbb"
+c="ccc"
+final=a+b+c
+print final
+
+
