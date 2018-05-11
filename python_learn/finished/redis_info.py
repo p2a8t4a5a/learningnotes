6ddf606df4d161508f9e7a94dec98fb4505f42d8
dongen.yao <dongen.yao@cootek.cn>
2018-05-11 13:40:49 +0800

    update

---
+++ python_learn/finished/redis_info.py
@@ -0,0 +1,23 @@
+内存调查笔记:
+
+
+pyrasite-shell 11
+
+import gc
+gc.set_debug(gc.DEBUG_COLLECTABLE)
+len(gc.garbage)
+
+gc.set_debug(gc.DEBUG_COLLECTABLE)
+len(gc.garbage)
+
+
+
+apt-get update
+apt-get install  gdb
+安装 meliae
+pip install  urwid
+pip install pyrasite
+
+
+pyrasite-memory-viewer 11
+
