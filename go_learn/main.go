b999a0ee6d00579b1e5eafbbe9fab26941bab91d
dongen.yao <dongen.yao@cootek.cn>
2019-07-24 08:15:44 +0800

    update

--- go_learn/main.go
+++ go_learn/main.go
@@ -66,9 +66,122 @@ func TestMemory() string {
     return string(res)
 }
 
+func TestMap() {
+    m := make(map[string]int)
+    m["a"] = 123
+    m["b"] = 456
+    fmt.Println(m)
+}
+
+
+func increment(i *int) {
+      *i++
+}
+
+func TestPoint() {
+    a:=1
+    increment(&a)
+    fmt.Println(a)
+    fmt.Println(*(&a))
+}
+
+func TestIF() {
+    if num:=1; num<0 {
+        fmt.Println(num, "num < 0")
+    } else if num < 1 {
+        fmt.Println(num, "num = 0")
+    } else {
+        fmt.Println(num, "num > 0")
+    }
+}
+
+func SwitchMain() {
+    i := 3
+    switch i {
+        case 1:
+            fmt.Println(i, "num = 1")
+        case 2:
+            fmt.Println(i, "num = 2")
+        default:
+            fmt.Println(i, "num other ")
+    }
+}
+
+func TestLoop() int {
+    sum := 0
+    for i:=0; i<10 ; i++ {
+        fmt.Println(i, "num")
+        sum += i
+    }
+
+    i := 0
+    for i < 10 {
+        fmt.Println(i)
+        i += 1
+    }
+
+    for {
+        i+=1
+        if i>12 {
+            break
+        } else {
+            fmt.Println(i)
+        }
+    }
+
+    return sum
+}
+
+
+func TestAdd1(a int, b int) int {
+    return a+b
+}
+
+func TestAdd2(a int, b int) (c int) {
+    c = a+b
+    return
+}
+
+func TestMultiReturn() (a int, b string) {
+    a = 1 + 1
+    b = "asd"
+    return a,b
+}
+
+func TestFunc() {
+    fmt.Println(TestAdd1(1,2) == TestAdd2(1,2))
+    a,b := TestMultiReturn()
+    fmt.Println(a,b)
+}
+
+type person struct {
+    name string 
+    age int
+    gender string
+}
+
+func TestStruct() {
+    a := person{name: "Bob", age: 42, gender: "male"}
+    b := person{"Bob", 42, "male"}
+    fmt.Println(a.name)
+    fmt.Println(b.age)
+    p := &a
+    fmt.Println(p.gender)
+}
+
 func main() {
     // TestAppend()
     // TestCopy()
-    tmp := TestMemory()
-    fmt.Println(tmp)
+    // tmp := TestMemory()
+    // fmt.Println(tmp)
+    // TestMap()
+    // TestPoint()
+    // TestIF()
+    // SwitchMain()
+    // TestLoop()
+    // TestFunc()
+    // TestStruct()
+
+    // mark METHODS
+    // https://milapneupane.com.np/2019/07/06/learning-golang-from-zero-to-hero/
 }
