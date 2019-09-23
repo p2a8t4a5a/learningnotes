35536319fdff500c135aa861c0480beb5de1c25f
dongen.yao <dongen.yao@cootek.cn>
2019-09-23 21:09:43 +0800

    update

--- go_learn/test.go
+++ go_learn/test.go
@@ -12,7 +12,41 @@ func test() (num int, err error) {
     return
 }
 
+func test2() {
+    a := []int{1,2,3}
+    b := a[:]
+    println(a[2])
+    a[2] = 99
+    println(b[2])
+}
+
+func test3() {
+    a := make([]int, 2, 4)
+    a = append(a, 2)
+    for i:=0;i<100;i++ {
+        a = append(a, 3)
+    }
+    for i:=0;i<len(a);i++ {
+        println(a[i])
+    }
+    println(len(a))
+    println(cap(a))
+}
+
+func test4() {
+    a := 2
+    println(a)
+    switch a {
+        case 1:
+            println("aa")
+        case 2:
+            println("bb")
+
+    }
+}
+
 func main() {
-    a,b := test()
-    fmt.Println(a, b)
+    // a,b := test()
+    // fmt.Println(a, b)
+    test4()
 }
